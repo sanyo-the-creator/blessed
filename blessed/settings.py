8d385193924199d4a8e6da39dce01cea72b770b5
Jergus <jergussnahnican@gmail.com>
2022-10-02 21:07:29 +0200

    try 2 gitignore

--- blessed/settings.py
+++ blessed/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-#try of gitignore
+#try 2 of gitignore
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
