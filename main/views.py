963242e2bbeafce5c3b4f6966cde912ceddccfbc
Jergus <jergussnahnican@gmail.com>
2022-10-04 20:49:55 +0200

    productlook

--- main/views.py
+++ main/views.py
@@ -1,8 +1,11 @@
-from pickle import FALSE
+
 from django.shortcuts import render
 from .models import Products
 # Create your views here.
 from django.contrib.auth import get_user_model
+# from IPython.display import display
+# from PIL import Image
+# import wget
 
 def home(response):
     try:
@@ -32,6 +35,9 @@ def addProducts(response):
             p.description = response.POST.get("description")
             p.categories = response.POST.get("category")
             p.size = response.POST.get("size")
+            img = response.FILES.get("image")
+            # file=wget.download(img,response.POST.get("image"))
+            
             p.image = response.POST.get("image")
             p.checked = False
             p.save()
@@ -40,4 +46,9 @@ def addProducts(response):
 
     
     return render(response, "main/addProducts.html", {})
+def productLook(response, id):
+    pd =Products.objects.get(id=id)
+    
+    return render(response, "main/productLook.html", {"pd":pd})
+    
     
--- register/forms.py
+++ register/forms.py
@@ -2,7 +2,7 @@ from unittest.util import _MAX_LENGTH
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from phone_field import PhoneField
+
 
 
 class RegisterForm(UserCreationForm):
